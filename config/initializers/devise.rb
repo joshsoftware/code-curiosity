2005a888e85ac7cc7da4f60e2cbb3a93f27c2e09
Shailesh Patil <shailesh@joshsoftware.com>
2015-12-29 12:20:41 +0530

    included global var files in devise config as we need the omniauth token at the time of initialization

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,4 @@
+require "#{Rails.root.join("config/initializers/globals")}"
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
