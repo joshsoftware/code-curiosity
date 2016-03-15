79e5f339777e00657db99a0dfc1fa3392de2bc2c
Jiren <jirenpatel@gmail.com>
2016-03-15 16:53:10 +0530

    Launch Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,7 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to dashboard_path
-      return
-    end
   end
 
-  def leaderboard
-    @users = User.order(points: :desc).page(params[:page]).per(25)
+  def thanks
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,60 +8,7 @@ Rails.application.routes.draw do
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  resources :repositories, except: [:edit, :update] do
-    get 'sync'
-  end
-
-  resources :transactions
-
-  resources :users, except: [:destroy] do
-    get 'sync'
-  end
-
-  resources :activities, only: [:index] do
-    collection do
-      get 'commits'
-      get 'activities'
-    end
-  end
-
-  resources :judging, only: [] do
-    collection do
-      get 'commits'
-      get 'activities'
-      post 'rate/:type/:id', action: 'rate', as: :rate
-      get 'comments/:type/:id', action: 'comments', as: :comments
-      post 'comments/:type/:id', action: 'comment', as: :comment
-    end
-  end
-
-  namespace :admin do
-    resources :repositories do
-      member do
-        patch :add_judges
-        get :assign_judge
-      end
-    end
-    resources :users do
-      get 'mark_as_judge'
-    end
-    resources :judges
-    resources :rounds do
-      get :mark_as_close
-    end
-  end
-
-  # get '/subscriptions/:id' => 'subscriptions#subscribe', as: :subscription
-  get 'get_new_repos' => "dashboard#get_new_repos"
-  get  'dashboard(/:category)', to: 'dashboard#index', as: :dashboard
-  post 'score' => 'application#score'
-  post 'webhook' => 'dashboard#webhook'
-  get 'change_round/:id' => "dashboard#change_round", as: :change_round
-  get 'dashboard' => 'dashboard#index'
-  get 'leaderboard' => 'dashboard#leaderboard'
+  get 'thanks' => 'home#thanks'
 
   root 'home#index'
 end
