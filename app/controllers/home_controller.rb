206e385767b5f9a3ca67db6291d195eb7fedabec
Jiren <jirenpatel@gmail.com>
2016-04-23 20:11:48 +0530

    Fixed leaderboard view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,12 +7,10 @@ class HomeController < ApplicationController
   end
 
   def leaderboard
-    subscriptions = current_round.subscriptions
+    @subscriptions = current_round.subscriptions
                                  .where(:points.gt => 0)
                                  .order(points: :desc)
                                  .page(1).per(5)
-
-    @users = subscriptions.map(&:user)
 
     if user_signed_in?
       render layout: 'application'
