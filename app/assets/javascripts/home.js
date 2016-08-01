1cde3b700805f84b18b23e7dca68ab76e2a88217
Sethupathi Asokan <sethu@joshsoftware.com>
2016-08-01 13:53:50 +0530

    refs #51 split the charts and removed the carousel for widgets

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,54 +1,23 @@
-$(document).ready(function() {
-  var size = $('.carousel .item').size();
-  $('.carousel .item').each(function(){
-
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    if (!next.children(':first-child').attr('class').includes('glyphicon')) {
-      next.children(':first-child').clone().appendTo($(this));
-    } else {
-      next = $('.carousel .item').first();
-      next.children(':first-child').clone().appendTo($(this));
-    }
-  
-    if (next.next().length > 0) {
-        if (!next.next().children(':first-child').attr('class').includes('glyphicon')) {
-          next.next().children(':first-child').clone().appendTo($(this));
-	} else {
-	  next = $('.carousel .item').first();
-	  next.next().children(':first-child').clone().appendTo($(this));
-	} 
-    } 
-    else {
-	  if (size > 3){
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-	  }
-    }
-  });
-});
-
-function multiLineChart(xAxis, users, contributions, redemptions){
-  $('#points-chart-container').highcharts({
+function lineChart(xAxis, trendData, title, subTitle, divId, legendName, lineColor){
+  $(divId).highcharts({
     chart: {
-      type: 'bar'
+      type: 'line'
     },
     title: {
-      text: 'CodeCuriosity Usage Trend',
+      text: title,
       x: -20 //center
     },
     subtitle: {
-      text: 'Last Six Months',
+      text: subTitle,
       x: -20
     },
     xAxis: {
       categories: xAxis
     },
     plotOptions: {
-      bar: {
+      line: {
         dataLabels: {
-          enabled: true
+          enabled: false
         }
       }
     },
@@ -72,26 +41,11 @@ function multiLineChart(xAxis, users, contributions, redemptions){
       borderWidth: 0
     },
     series: [{
-      name: 'Users',
-      data: users,
+      name: legendName,
+      data: trendData,
       marker: { enabled: false },
-      color: '#3C8DBC',
+      color: lineColor,
       dataLabels: {color:'#3C8DBC',style: {"color": "contrast", "fontSize": "11px", "fontWeight": "bold", "textShadow": "0" }}
-    }, {
-      name: 'Contributions',
-      data: contributions,
-      marker: { enabled: false },
-      color: '#00A65A',
-      dataLabels: {color:'#00A65A',style: {"color": "contrast", "fontSize": "11px", "fontWeight": "bold", "textShadow": "0" }}
-    }, {
-      name: 'Redemptions',
-      data: redemptions,
-      marker: { enabled: false },
-      color: '#F39C12',
-      tooltip: {
-        valuePrefix: '$'
-      }, 
-      dataLabels: {color:'#F39C12',style: {"color": "contrast", "fontSize": "11px", "fontWeight": "bold", "textShadow": "0" }}
     }] 
   });  
 } 
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -27,49 +27,52 @@ module HomeHelper
     #Group data by month
     users = User.where(:created_at.gt => START_OF_MONTH_FOR_HOME_PAGE_REPORT, :created_at.lte => Date.today, auto_created: false).group_by{|u| u.created_at.strftime(REPORT_DATE_FORMAT)}
     contributions = Transaction.where(:created_at.gt => START_OF_MONTH_FOR_HOME_PAGE_REPORT, :created_at.lte => Date.today, :transaction_type.in => ['royalty_bonus', 'Round']).group_by{|u| u.created_at.strftime(REPORT_DATE_FORMAT)}
-    redemptions = Transaction.where(:created_at.gt => START_OF_MONTH_FOR_HOME_PAGE_REPORT, :created_at.lte => Date.today, :transaction_type => 'redeem_points').group_by{|u| u.created_at.strftime(REPORT_DATE_FORMAT)}
+    #redemptions = Transaction.where(:created_at.gt => START_OF_MONTH_FOR_HOME_PAGE_REPORT, :created_at.lte => Date.today, :transaction_type => 'redeem_points').group_by{|u| u.created_at.strftime(REPORT_DATE_FORMAT)}
 
     @user_trend = []
     @contribution_trend = []
     @redemption_trend = []
     @xAxis = []
-    users.map{ |user| @user_trend << user[1].size}
-    contributions.map{ |contribution| @contribution_trend << contribution[1].sum(&:points)}
-    redemptions.map{ |redemption| @redemption_trend << (redemption[1].sum(&:points).abs/REDEEM['one_dollar_to_points'])}
-    
+
     i = START_MONTH
     while(i >= 0) do
       @xAxis << i.months.ago.beginning_of_month.strftime(REPORT_DATE_FORMAT)
       i = i - 1
     end
-=begin
-    #Find the largest xAxis among the three 
-    if users.keys.size > contributions.keys.size
-      @xAxis = users.keys
-      if redemptions.keys.size > users.keys.size 
-        @xAxis = redemptions.keys
-      end 
-    else
-      @xAxis = contributions.keys
-      if redemptions.keys.size > users.keys.size
-        @xAxis = redemptions.keys
+
+    
+    sum = 0
+    #cummulative sum
+    users.map{ |user| @user_trend << sum += user[1].size}
+    
+    sum = 0
+    contributions.map{ |contribution| @contribution_trend << sum += contribution[1].sum(&:points)}
+    
+    sum = 0
+    #redemptions.map{ |redemption| @redemption_trend << sum += (redemption[1].sum(&:points).abs/REDEEM['one_dollar_to_points'])}
+
+    index = 0
+    #Fill in 0 for those don't have any value for that specific month, otherwise the report will show mismatched axis values.
+    @xAxis.each{ |value|
+      if !users.keys.include?(value)
+        if index == 0
+          @user_trend.insert(index,0)
+        elsif
+	  @user_trend.insert(index,@user_trend[index-1])
+        end
       end
-   end
-=end
-   index = 0
-   #Fill in 0 for those don't have any value for that specific month, otherwise the report will show mismatched axis values.
-   @xAxis.each{ |value|
-     if !users.keys.include?(value)
-       @user_trend.insert(index,0)
-     end
-     if contributions.keys.index(value).blank?
-       @contribution_trend.insert(index,0)
-     end
-     if redemptions.keys.index(value).blank?
-       @redemption_trend.insert(index,0)
-     end
-     index = index + 1
-   }
+      if contributions.keys.index(value).blank?
+        if index == 0
+          @contribution_trend.insert(index,0)
+        elsif
+          @contribution_trend.insert(index,@contribution_trend[index-1])
+        end
+      end
+      #if redemptions.keys.index(value).blank?
+        #@redemption_trend.insert(index,0)
+      #end
+      index = index + 1
+    }
  end
 
 end
