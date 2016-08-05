31cdc9a37b34357bb55474492ccf17775a8cdb48
BandanaPandey <BandanaPandey@users.noreply.github.com>
2016-08-05 14:07:12 +0530

    refs#55 Added filter in Redeem_requests (#67)

--- app/assets/javascripts/admin/redeem_requests.js
+++ app/assets/javascripts/admin/redeem_requests.js
@@ -10,4 +10,31 @@ $(document).on('page:change', function(event) {
     modal.find('#redeem_request_points').val(button.data('points'));
     modal.find('#redeem_request_status').val(button.data('status') + '');
   })
+
+  $(function() {
+    $('.check').bootstrapToggle();
+  })
+  
+  $('.check').change(function() {
+    if($(this).is(':checked')){
+      console.log(this.checked);
+      $.ajax({
+        type: 'get',
+        url: '/admin/redeem_requests', 
+        data: {'status': false}
+      })
+    }
+    else{
+      console.log($(this).is(':checked'));
+      $.ajax({
+        type: 'get',
+        url: '/admin/redeem_requests',
+        data: {'status': true}
+      })
+    }
+
+  })
+    
+
+
 });
