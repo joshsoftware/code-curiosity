1bedd3176aec39435008ce741d3deecef7f56a9c
BandanaPandey <BandanaPandey@users.noreply.github.com>
2016-08-24 18:40:33 +0530

    Modified ReedemRequest Model for delivering redemption mails to user (#96)

--- app/mailers/redeem_mailer.rb
+++ app/mailers/redeem_mailer.rb
@@ -6,7 +6,6 @@ class RedeemMailer < ApplicationMailer
     @points = request.points
 
     mail(to: @user.email, subject: "[CODECURIOSITY] #{@points} points redemption request")
-    notify_admin(request)
   end
 
   def notify_admin(request)
