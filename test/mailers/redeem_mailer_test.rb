cb10ad3b24334c423a06ed2af5499113f27bd542
BandanaPandey <BandanaPandey@users.noreply.github.com>
2016-08-26 11:54:40 +0530

    refs#13 Added testcases for redeem_requests_controller, redeem_controller, redeem_mailer, redeem_request_model (#99)

--- test/mailers/redeem_mailer_test.rb
+++ test/mailers/redeem_mailer_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class RedeemMailerTest < ActionMailer::TestCase
-  def test_sanity
-    flunk "Need real tests"
+  include ActiveJob::TestHelper 
+
+  def test_mail_is_enqueued_to_be_delivered_later
+    user = create(:user)
+    transaction = create(:transaction, :type => 'credit', :points => 20, user: user)
+    r = create(:redeem_request, :points => 2, user: user)
+    assert_enqueued_jobs 1 do 
+      RedeemMailer.redeem_request(r).deliver_later
+    end 
   end
+
+  def test_mail_should_be_delivered
+    user = create(:user)
+    transaction = create(:transaction, :type => 'credit', :points => 20, user: user)
+    r = create(:redeem_request, :points => 2, user: user)
+    assert_difference 'ActionMailer::Base.deliveries.size', +1 do
+      RedeemMailer.redeem_request(r).deliver_now
+    end
+  end
+
+  def test_mail_is_delivered_with_expected_content 
+    user = create(:user)
+    transaction = create(:transaction, :type => 'credit', :points => 20, user: user)
+    r = create(:redeem_request, :points => 2, user: user)
+    perform_enqueued_jobs do 
+      mail = RedeemMailer.redeem_request(r).deliver_now
+      delivered_email = ActionMailer::Base.deliveries.last
+      assert_includes delivered_email.to, mail.to.first 
+    end
+  end 
+
 end
