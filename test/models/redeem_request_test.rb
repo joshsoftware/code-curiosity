267ac71ad578dfe5e1b09350503aed27517cb0b1
Bandana <bandanapandey11@gmail.com>
2016-09-06 13:07:45 +0530

    refs#13 Added more testcases for redeem_request controller and redeem_request model

--- test/models/redeem_request_test.rb
+++ test/models/redeem_request_test.rb
@@ -1,74 +1,75 @@
 require "test_helper"
 
 class RedeemRequestTest < ActiveSupport::TestCase
+  include ActiveJob::TestHelper 
 
   def redeem_request
     @redeem_request ||= build(:redeem_request)
   end
 
-  def test_validity_of_redeem_request
+  test "validity of redeem request" do
     redeem_request = build(:redeem_request, :points => 2, :address => 'baner')
     assert_not redeem_request.valid?
   end
 
-  def test_address_must_be_present_when_retailer_is_other
+  test "address must be present when retailer is other" do
     redeem_request = build(:redeem_request, :points => 2, :retailer => 'other', :gift_product_url => Faker::Internet.url)
     redeem_request.valid?
     assert_not_empty redeem_request.errors[:address]
   end
 
-  def test_gift_produt_url_must_be_present_when_retailer_category_is_other
+  test "gift_produt_url must be present when retailer category is other" do
     redeem_request = build(:redeem_request, :points => 2, :retailer => 'other', :address => 'pune')
     redeem_request.valid?
     assert_not_empty redeem_request.errors[:gift_product_url]
   end
 
-  def test_no_updation_of_transaction_points_when_point_is_zero
+  test "no updation of transaction points when point is zero" do
     transaction = create(:transaction, :points => 3, :type => 'credit')
     redeem_request = build(:redeem_request, :points => 0, :address => 'baner', transaction: transaction)
     redeem_request.update_transaction_points
     assert_equal redeem_request.transaction.points, 3
   end
 
-  def test_update_transaction_points_only_when_points_greater_than_zero
+  test "update transaction_points only when points greater than zero" do
     transaction = create(:transaction, :points => 3, :type => 'credit')
     redeem_request = build(:redeem_request, :points => 2, :address => 'baner', transaction: transaction)
     redeem_request.update_transaction_points
     assert_equal redeem_request.transaction.points, 2
   end
 
-  def test_whether_retailer_category_is_other
+  test "whether retailer category is other" do
     redeem_request = build(:redeem_request, :points => 2, :address => 'baner', :retailer => 'other', :gift_product_url => Faker::Internet.url, :address => 'baner')
     assert redeem_request.retailer_other?
   end
 
-  def test_user_total_points_must_be_greater_than_or_equal_to_redeemption_points
+  test "user total_points must be greater than or equal to redeemption_points" do
     user = create(:user)
     transaction = create(:transaction, :points => 4, :type => 'credit', user: user)
     redeem_request = create(:redeem_request, :points => 3, :address => 'baner', :retailer => 'github', user: user)
     assert_empty redeem_request.errors[:points]
   end
 
-  def test_for_redeemption_points_must_be_greater_than_zero
+  test "for redeemption points must be greater than zero" do
     redeem_request = build(:redeem_request, :points => 0, :retailer => 'amazon', user: create(:user_with_transactions))
     redeem_request.save
     assert_not_empty redeem_request.errors[:points]
   end
 
-  def test_points_not_in_mutiple_of_hundred_so_no_redeemption
+  test "points not in mutiple of_hundred so no redeemption" do
     redeem_request = build(:redeem_request, :points => 2, :retailer => 'amazon', user: create(:user_with_transactions))
     redeem_request.save
     assert_not_empty redeem_request.errors[:points]
   end
 
-  def test_points_must_be_in_multiple_of_hundred_for_redeemption
+  test "points must be in multiple of hundred for redeemption" do
     user = create(:user)
     transaction = create(:transaction, :points => 100, :type => 'credit', user: user)
     redeem_request = create(:redeem_request, :points => 2, user: user)
     assert redeem_request.valid?
   end
 
-  def test_creating_redeem_request_must_create_redeem_transaction
+  test "creating redeem_request must create redeem_transaction" do
     user = create(:user, :points => 3)
     transaction = create(:transaction, :type => 'credit', :points => 5, user: user)
     redeem_request = create(:redeem_request, :points => 2, user: user)
@@ -76,7 +77,7 @@ class RedeemRequestTest < ActiveSupport::TestCase
     assert_equal transaction_type, 'redeem_points'
   end 
   
-  def test_transaction_corresponding_to_redeem_request_must_be_destroyed_when_it_is_deleted
+  test "transaction corresponding to redeem_request must be destroyed when it is deleted" do
     user = create(:user)
     transaction = create(:transaction, :type => 'credit', :points => 5, user: user)
     redeem_request = create(:redeem_request, :points => 1, user: user)
@@ -84,4 +85,40 @@ class RedeemRequestTest < ActiveSupport::TestCase
       redeem_request.destroy
     end
   end
+
+  test "transaction corresponding to redeem request must be destroyed when it is deleted" do
+    user = create(:user)
+    assert_equal user.transactions.count, 0
+    transaction = create(:transaction, :type => 'credit', :points => 4, user: user)
+    assert_equal user.transactions.count, 1
+    redeem_request = create(:redeem_request, :points => 1, user: user)
+    assert_equal user.redeem_requests.count, 1
+    assert_equal user.transactions.count, 2
+    redeem_request.destroy
+    assert_equal user.redeem_requests.count, 0
+    assert_equal user.transactions.count, 1
+  end
+
+  test "send notification only when coupon_code or comment is changed" do
+    user = create(:user)
+    assert_equal user.transactions.count, 0
+    transaction = create(:transaction, :type => 'credit', :points => 4, user: user)
+    assert_equal user.transactions.count, 1
+    assert_enqueued_jobs 3 do 
+      redeem_request = create(:redeem_request, :points => 1, :coupon_code => 'abc', user: user)
+    end
+  end
+
+  test "redeem request must be updated when coupon_code or comment_changed" do
+    user = create(:user)
+    assert_equal user.transactions.count, 0
+    transaction = create(:transaction, :type => 'credit', :points => 4, user: user)
+    assert_equal user.transactions.count, 1
+    redeem_request = create(:redeem_request, :points => 1, user: user)
+    assert_equal user.transactions.count, 2
+    redeem_request.coupon_code = 'Josh12'
+    redeem_request.save!
+    assert_equal redeem_request.coupon_code, 'Josh12'
+  end
+  
 end
