05625ab0ff271b8de188838853e101ac9c061711
Sethupathi Asokan <sethu@joshsoftware.com>
2016-09-08 15:39:24 +0530

    ref #51 changed the both homepage trend charts data to obtain directly for mongodb aggregator

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,8 +1,5 @@
 module HomeHelper
-  #Fetch only the last 6 months data from today
-  START_MONTH = 5
-  START_OF_MONTH_FOR_HOME_PAGE_REPORT = START_MONTH.months.ago.beginning_of_month
-  REPORT_DATE_FORMAT = "%B-%Y"
+  
   def featured_groups
     @featured_groups =  Group.where(is_featured: true)
   end
@@ -21,27 +18,21 @@ module HomeHelper
     end
   end
 
-  #Need to create a rake and cron job which will extract the required data every month and store it in a collection. 
-  #Those data can be directly fetched to render the report
   def multi_line_chart
-    #Group data by month
-    users = User.where(:created_at.gt => START_OF_MONTH_FOR_HOME_PAGE_REPORT, :created_at.lte => Date.today, auto_created: false).group_by{|u| u.created_at.strftime(REPORT_DATE_FORMAT)}
+   
+    users = Subscription.collection.aggregate( [  { "$group" => { _id: "$round_id", total: { "$sum" => 1 } } } ] ).sort {|x, y| Date.parse(Round.find(y["_id"]).name) <=> Date.parse(Round.find(x["_id"]).name) }.collect { |r| [ Round.find(r["_id"]).name, r["total"] ] }[0..5].reverse
 
-    contributions = Subscription.collection.aggregate( [ {"$match" => { "created_at" => { "$gt" => Date.parse("march 2016") } } }, { "$group" => { _id: "$round_id", total: { "$sum" => "$points" } } } ]).collect { |r| [ Round.find(r["_id"]).from_date.strftime("%b %Y"), r["total"] ] }.reverse
+    contributions = Subscription.collection.aggregate( [ {"$match" => { "created_at" => { "$gt" => Date.parse("march 2016") } } }, { "$group" => { _id: "$round_id", total: { "$sum" => "$points" } } } ]).sort {|x, y| Date.parse(Round.find(y["_id"]).name) <=> Date.parse(Round.find(x["_id"]).name) }.collect { |r| [ Round.find(r["_id"]).name, r["total"] ] }[0..5].reverse
 
     @user_trend = []
     @contribution_trend = []
-    @redemption_trend = []
+    @user_xAxis = []
     @xAxis = []
     
-    sum = 0
-    #cummulative sum
-    users.map{ |user| @user_trend << sum += user[1].size}
+    users.map{ |user| @user_trend << user[1]; ; @user_xAxis << user[0]}
     
-    sum = 0
     contributions.map{ |contribution| @contribution_trend << contribution[1]; @xAxis << contribution[0]}
     
-    sum = 0
 
  end
 
