948019c15e72b17a4c38169a4f128e02d5ba5d29
Sethupathi Asokan <sethu@joshsoftware.com>
2016-09-09 15:58:23 +0530

    Added performance chart on user dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,12 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!, except: [:webhook]
 
   def index
+    subscriptions = current_user.subscriptions.where(:created_at.gt => Date.parse("Feb 2016")).asc(:created_at)
+    @xAxis = []
+    @commits = []
+    @activities = []
+    @points = []
+    subscriptions.map{|s| @xAxis << s.round.name; @commits << s.commits_count; @activities << s.activities_count; @points << s.points}
   end
 
   def change_round
