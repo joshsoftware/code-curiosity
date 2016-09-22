f0fee629f11f30592623e6d1d469b7bff2cb671e
Prasad Surase <prasadsurase@gmail.com>
2016-09-22 12:55:50 +0530

    Updated the specs for Admin::DashboardController

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -1,16 +1,36 @@
 require "test_helper"
 
 class Admin::DashboardControllerTest < ActionController::TestCase
-  def test_index
-    admin_role = create :role, :admin
-    goal = create :goal, points: 15
-    round = create :round, :open
-    user = create :user, auth_token: 'dah123rty', goal: goal
-    user.roles << admin_role
-    sign_in user
+
+  test 'non logged-in user accesses index page' do
+    setup_data
+    get :index
+    assert_response :redirect
+  end
+
+  test 'logged-in user accesses index page' do
+    setup_data
+    sign_in @user
+    get :index
+    assert_response :redirect
+  end
+
+  test 'admin user accesses index page' do
+    setup_data
+    @user.roles << @admin_role
+    sign_in @user
     get :index
     assert_response :success
     assert_template :index
+  end
+
+  private
+
+  def setup_data
+    @admin_role = create :role, :admin
+    @goal = create :goal, points: 15
+    @round = create :round, :open
+    @user = create :user, auth_token: 'dah123rty', goal: @goal
   end
 
 end
