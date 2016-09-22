a4d38b4eab1565f372257652bca36b39456faac4
Prasad Surase <prasadsurase@gmail.com>
2016-09-22 12:29:46 +0530

    Added test cases for Admin::DashboardController and ReposHelper

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -2,8 +2,15 @@ require "test_helper"
 
 class Admin::DashboardControllerTest < ActionController::TestCase
   def test_index
+    admin_role = create :role, :admin
+    goal = create :goal, points: 15
+    round = create :round, :open
+    user = create :user, auth_token: 'dah123rty', goal: goal
+    user.roles << admin_role
+    sign_in user
     get :index
     assert_response :success
+    assert_template :index
   end
 
 end
