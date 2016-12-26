7c652f83f537032eedbad83075e5865fcdd8fc9b
Prasad Surase <prasadsurase@gmail.com>
2016-12-26 15:17:32 +0530

    Added reactjs component for Points tile on dashboard page.

---
+++ app/assets/javascripts/components/points_tile.js.jsx
@@ -0,0 +1,19 @@
+var PointsTile = React.createClass({
+
+  render: function() {
+    return (
+      <div className={`small-box ${this.props.color}`}>
+        <div className="inner">
+          <h3>{ this.props.points }</h3>
+          <p>{ this.props.title }</p>
+        </div>
+        <div className="icon">
+          <i className={`ion ${this.props.logo}`}></i>
+        </div>
+        <a className="small-box-footer" href={ this.props.path }>More info
+          <i className="fa fa-arrow-circle-right"></i>
+        </a>
+      </div>
+    );
+  }
+});
