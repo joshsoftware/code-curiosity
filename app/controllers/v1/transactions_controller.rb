2a8500c0ccd4a1a200c302fadbe9c9b4315b714a
Prasad Surase <prasadsurase@gmail.com>
2016-12-28 14:30:26 +0530

    Displayed all the transaction of the current user in points history page.

--- app/controllers/v1/transactions_controller.rb
+++ app/controllers/v1/transactions_controller.rb
@@ -2,7 +2,6 @@ class V1::TransactionsController < V1::BaseController
   before_action :authenticate_user!
 
   def index
-    @records_per_page = Kaminari.config.default_per_page
-    render json: Transaction.where(user_id: current_user.id).desc(:created_at).page(params[:page]).per(@records_per_page)
+    render json: current_user.transactions.desc(:created_at)
   end
 end
