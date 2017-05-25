66edc07b5cd8995949574dd9901980e714f81880
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-05-25 18:42:17 +0530

    Changes in SponsorerDetail model[#276]
    changes the type of payment plan from integer to string, adds some extra fields for subscription and changes in validations

--- app/models/sponsorer_detail.rb
+++ app/models/sponsorer_detail.rb
@@ -3,8 +3,12 @@ class SponsorerDetail
   include Mongoid::Paperclip
 
   field :sponsorer_type, type: String
-  field :payment_plan, type: Integer
+  field :payment_plan, type: String
   field :publish_profile, type: Boolean, default: false
+  field :stripe_customer_id, type: String
+  field :stripe_subscription_id, type: String
+  field :subscribed_at, type: DateTime
+  field :subscription_expires_at, type: DateTime
 
   has_mongoid_attached_file :avatar, 
     path: ':rails_root/public/system/sponsorer/:id/:style/:filename',
@@ -13,16 +17,16 @@ class SponsorerDetail
 
   belongs_to :user
 
-  validates :sponsorer_type, :user, presence: true
+  validates :sponsorer_type, :user, :payment_plan, :stripe_customer_id, :stripe_subscription_id, presence: true
   validates :sponsorer_type, :inclusion => { :in => ['INDIVIDUAL','ORGANIZATION'] }
-  validates :payment_plan, numericality: { only_integer: true, greater_than: 0 }
+  validates :payment_plan, :inclusion => { :in => SPONSOR['individual'].keys }
   # validates :publish_profile, :inclusion => { :in => [true, false] }
   # validates_attachment :avatar, presence: true,
   #   content_type: { content_type: /\Aimage\/.*\Z/ },
   #   file_name: { matches: [/png\Z/, /jpe?g\Z/] }
   #   # size: { in: 0..10.kilobytes }
   # validate attachment presence
-  validates_attachment_presence :avatar
+  # validates_attachment_presence :avatar
   # Validate filename
   validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
   # Validate content type
@@ -34,8 +38,8 @@ class SponsorerDetail
 
   def update_user_as_sponsor
     @user = self.user
-    # @role = Role.find_or_create_by(name: 'Sponsorer')
-    # @user.roles << @role
+    @role = Role.find_or_create_by(name: 'Sponsorer')
+    @user.roles << @role unless @user.is_sponsorer?
     @user.set({is_sponsorer: true})
   end
 end
