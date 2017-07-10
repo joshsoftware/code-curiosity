5e5ab718f3c0bdc695288e1d4337cdfe2d63d2fc
amitk <amit@joshsoftware.com>
2017-07-10 18:52:05 +0530

    Sum of all Redemption Requests [#296]
    A helper method added to display the capital value of total redeemable points.

--- app/helpers/admin/redeem_requests_helper.rb
+++ app/helpers/admin/redeem_requests_helper.rb
@@ -1,7 +1,7 @@
 module Admin::RedeemRequestsHelper
 
   def total_capital_of_points
-    RedeemRequest.total_points_redeemed/10
+    RedeemRequest.where(status: false).sum(:points)/10
   end
   
 end
