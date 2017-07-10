8dbe9ed5f1f3d97c55cef739f2a0cf76cd2acd1a
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-07-10 07:42:16 +0530

    Handles stripe events[#276]

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -12,6 +12,12 @@ class StripeController < ApplicationController
         subscriber = SponsorerDetail.find_by(stripe_customer_id: event_object['customer'])
         subscriber.subscribed_at = Time.at(event_object['start']).to_datetime
         subscriber.subscription_expires_at = Time.at(event_object['current_period_end']).to_datetime
+        subscriber.subscription_status = event_object['status']
+        subscriber.save
+      when 'invoice.payment_succeeded'
+        #send a mail regarding successfull payment and update expiry date
+        subscriber = SponsorerDetail.find_by(stripe_customer_id: event_object['customer'])
+        subscriber.subscription_expires_at = Time.at(event_object['current_period_end']).to_datetime
         subscriber.save
       when 'invoice.payment_failed'
         #send a mail regarding unsuccessfull payment
@@ -21,6 +27,21 @@ class StripeController < ApplicationController
           user_id = SponsorerDetail.find_by(stripe_customer_id: event_object['customer']).user_id
           SponsorMailer.subscription_payment_failed(user_id.to_s, message).deliver_later
         end
+      when 'customer.subscription.updated'
+        subscriber = SponsorerDetail.find_by(stripe_customer_id: event_object['customer'])
+        subscriber.subscription_status = event_object['status']
+        subscriber.save
+      when 'customer.source.created'
+        subscriber = SponsorerDetail.find_by(stripe_customer_id: event_object['customer'])
+        if subscriber.subscription_status == 'unpaid'
+          latest_invoice = Stripe::Invoice.list(:customer => subscriber.stripe_customer_id).data.first
+          invoice.update(closed: false)
+          invoice.pay
+        end
+      when 'customer.subscription.deleted'
+        @sponsor = SponsorerDetail.find_by(stripe_customer_id: event_object['customer'])
+        @sponsor.subscription_status = event_object['status']
+        @sponsor.save
     end
     rescue Exception => ex
       render :json => {:status => 422, :error => "Webhook call failed"}
