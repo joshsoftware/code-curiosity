3ef1fcda97cf3e950ed11afeda5844de1ed6b233
amitk <amit@joshsoftware.com>
2017-09-19 17:04:12 +0530

    Admin repositories page modified
    Displaying only parent repositories and those forked repositories whose parent not present in DB.
    Forked repos count present in DB with parent repos.
    Ignore all forked repos if parent repo is ignored.

--- app/controllers/admin/repositories_controller.rb
+++ app/controllers/admin/repositories_controller.rb
@@ -2,11 +2,11 @@ class Admin::RepositoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin!
   before_action :load_repository, only: [ :assign_judge, :add_judges, :update_ignore_field ]
-
   def index
     status = params[:ignored] || false
-    @repos = Repository.where(ignore: status, name: /#{params[:query]}/).
+    @repos = Repository.parent.where(ignore: status, name: /#{params[:query]}/).
     order(name: :asc).page(params[:page])
+    load_forks_count
     if request.xhr?
       respond_to do |format|
         format.js
@@ -25,6 +25,8 @@ class Admin::RepositoriesController < ApplicationController
 
   def update_ignore_field
     @repo.update_attributes(ignore: params[:ignore_value])
+    repositories = Repository.any_of({popular_repository_id: @repo.id}, {source_gh_id: @repo.gh_id})
+    repositories.update_all(ignore: params[:ignore_value]) if repositories
   end
 
   def search
@@ -33,13 +35,17 @@ class Admin::RepositoriesController < ApplicationController
       return
     end
 
-    @repos = Repository.required.where(name: /#{params[:q]}/).asc(:name).page(params[:page])
-
+    @repos = Repository.required.parent.where(name: /#{params[:q]}/).asc(:name).page(params[:page])
+    load_forks_count
     render :index
   end
 
+  def load_forks_count
+    @forks_repos_count = Repository.collection.aggregate [ { '$match' => {popular_repository_id: { '$in' => @repos.pluck(:id) }  } }, {'$group' => { '_id' => '$popular_repository_id', 'count' => { '$sum' => 1} } } ]
+  end
+
   private
-   
+
   def load_repository
     @repo = Repository.find(params[:id])
   end
