fe9f82da8e43dc0bf2cb256ac1a7b0f22505fd20
Rahul Jadhav <rahul.rj9421@gmail.com>
2018-05-25 18:21:21 +0530

    change app_num to app_credentials_counter

--- app/models/git_app.rb
+++ app/models/git_app.rb
@@ -1,21 +1,18 @@
-require 'github_client'
-require 'git_lib_ext'
-
 class GitApp
   include Mongoid::Document
 
-  @@app_num = 1
+  @@app_credentials_counter = 1
 
-  def self.app_num
-    @@app_num
+  def self.app_credentials_counter
+    @@app_credentials_counter
   end
 
-  def self.app_num=(num)
-    app_num = num
+  def self.app_credentials_counter=(number)
+    @@app_credentials_counter = number
   end
 
   def self.info
-    info = GIT_INFO['App_' + app_num.to_s]
+    info = GIT_INFO["App_#{app_credentials_counter}"]
     Github.new(
                oauth_token: info['access_tokens'],
                client_id: info['git_app_id'],
@@ -24,7 +21,7 @@ class GitApp
   end
 
   def self.inc
-    app_num += 1
-    app_num = 1 if app_num > 10
+    self.app_credentials_counter += 1
+    self.app_credentials_counter = 1 if self.app_credentials_counter > 10
   end
 end
