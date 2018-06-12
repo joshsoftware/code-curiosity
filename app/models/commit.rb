a363b7444fa28a11c1ad9a5706d2853c9c45d978
Rahul Jadhav <rahul.rj9421@gmail.com>
2018-06-12 17:13:02 +0530

    Feature to reveal points of commits

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -12,12 +12,10 @@ class Commit
   field :html_url,         type: String
   field :comments_count,   type: Integer, default: 0
   field :sha,              type: String
-  field :auto_score,       type: Integer
-  field :default_score,    type: Float, default: 0
-  field :bugspots_score,   type: Float, default: 0
   field :score,            type: Float, default: 0
   field :reward,           type: Float, default: 0
   field :frequency_factor, type: Float, default: 1
+  field :is_reveal,        type: Boolean, default: false
 
   belongs_to :user
   belongs_to :repository
@@ -38,7 +36,7 @@ class Commit
   index({ repository_id: 1 })
   index({ commit_date: -1 })
   index({ sha: 1 })
-  index(  auto_score: 1)
+  index(  score: 1)
 
   after_create do |c|
     c.user.inc(commits_count: 1)
