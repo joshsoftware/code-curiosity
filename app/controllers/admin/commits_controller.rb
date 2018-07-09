9e7e0408c291f5f0b3ad44e6dfca4303a25fbcc8
tanya-saroha <tanya@joshsoftware.com>
2018-07-09 09:57:41 +0530

    Fixed test cases for commits controller

--- app/controllers/admin/commits_controller.rb
+++ app/controllers/admin/commits_controller.rb
@@ -1,10 +1,10 @@
 class Admin::CommitsController < ApplicationController
   
   def index
-    from = params[:from].presence.try(:to_date).try(:beginning_of_day) || Date.yesterday.beginning_of_day
-    to = params[:to].presence.try(:to_date).try(:end_of_day) || Date.yesterday.end_of_day
-    @commits = Commit.all
-    @commits = @commits.in_range(from, to)
+    from = (params[:from].presence.try(:to_date) || Date.yesterday).beginning_of_day
+    to = (params[:to].presence.try(:to_date) || Date.yesterday).end_of_day
+
+    @commits = Commit.all.in_range(from, to)
                        .search_by(params[:query])
                        .page(params[:page])
   end
--- test/controllers/admin/commits_controller_test.rb
+++ test/controllers/admin/commits_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 describe CommitsController do
   before do
     user  = create :user, :auth_token => 'abc123'
+    user.name = 'user'
+    repository = create :repository
+    user.repositories << repository
     5.times.each do |i|
       create :commit, user: user, commit_date: Date.yesterday, message: "commit_#{i}"
     end
@@ -18,36 +21,49 @@ describe CommitsController do
     assert_equal assigns(:commits).count, 5
   end
 
+  describe 'date range is provided' do
+    describe 'commits are present' do
+      test 'should display commits between two dates' do
+        get :index, {from: Date.yesterday - 1, to: Date.yesterday}
+        assert_equal assigns(:commits).count, 5
+      end
+
+      test 'should search commits when user name present in search query' do
+        get :index, {query: 'user'}
+        assert_equal assigns(:commits).count, 5
+      end
+
+      test 'should search commits when repository name present in search query' do
+        get :index, {query: 'repository'}
+        assert_equal assigns(:commits).count, 0
+      end
+
+      test 'should search commits when description present in search query' do
+        get :index, {query: '1'}
+        assert_equal assigns(:commits).count, 1
+      end
+    end
+
+    describe 'no commits are present' do
+      test 'should return nothing' do
+        get :index, {from: Date.today, to: Date.today + 1}
+        assert_equal assigns(:commits).count, 0
+      end
+
+      test 'should give nothing when search query is present' do
+        get :index, {query: 'tanya'}
+        assert_equal assigns(:commits).count, 0
+      end
+    end
+  end
+
   describe 'date range is not provided' do
-    test "should display yesterday's commits" do
+    test 'should display commits of previous day' do
       get :index
       assert_equal assigns(:commits).count, 5
     end
   end
-
-  describe 'date range is provided' do
-    test 'should display commits between two dates: case 1' do
-      get :index, {from: Date.yesterday - 1, to: Date.yesterday}
-      assert_equal assigns(:commits).count, 5
-    end
-
-    test 'should display commits between two dates: case 2' do
-      get :index, {from: Date.today, to: Date.today + 1}
-      assert_equal assigns(:commits).count, 0
-    end
-  end
-
-  describe 'query is provided' do
-    test 'should search commits when search query present' do
-      get :index, {query: '1'}
-      assert_equal assigns(:commits).count, 1
-    end
-  
-    test 'should display commits between two dates and search by query' do
-      get :index, {from: Date.yesterday, to: Date.yesterday, query: 'commit'}
-      assert_equal assigns(:commits).count, 5
-    end
-  end
 end
+
 
 
