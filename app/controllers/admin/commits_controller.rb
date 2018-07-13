81f3303970f74a4a83f3aa475807c3c182432400
tanya-saroha <tanya@joshsoftware.com>
2018-07-13 10:41:23 +0530

    show admin commits from start of month till today

--- app/controllers/admin/commits_controller.rb
+++ app/controllers/admin/commits_controller.rb
@@ -1,8 +1,8 @@
 class Admin::CommitsController < ApplicationController
   
   def index
-    from = (params[:from].presence.try(:to_date) || Date.yesterday).beginning_of_day
-    to = (params[:to].presence.try(:to_date) || Date.yesterday).end_of_day
+    from = (params[:from].presence.try(:to_date) || Date.today.beginning_of_month).beginning_of_day
+    to = (params[:to].presence.try(:to_date) || Date.today).end_of_day
 
     @commits = Commit.all.in_range(from, to)
                        .search_by(params[:query])
