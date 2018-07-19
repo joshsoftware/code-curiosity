80086ef0bfaaa88691d5001d45833403177b4c47
tanya-saroha <tanya@joshsoftware.com>
2018-07-19 19:54:38 +0530

    Test for calculate score and reward rake task

---
+++ test/lib/tasks/score_and_reward_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ScoreAndRewardTest < ActiveSupport::TestCase
+  def test_score_and_reward_calculations
+      repo = create :repository, name: 'tanya-josh', owner: 'tanya-saroha', language: 'Ruby'
+      user = create  :user, github_handle: 'tanya-saroha', created_at: Date.yesterday - 1 
+      commit = create :commit, message: 'commit1', repository_id: repo.id
+
+      assert_equal commit.score, 0
+      assert_nil commit.reward
+
+      Rake::Task['score_and_reward'].invoke
+      commit.reload
+
+      assert commit.score > 5
+      assert commit.reward > 5
+    end
+end
